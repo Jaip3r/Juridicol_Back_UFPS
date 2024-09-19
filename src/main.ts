c34d807b55526ec8748b216b1dc889897aa34977
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-09-18 21:26:24 -0500

    Añadiendo seguridad de helmet y validación con class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+
+  // Prefijo global para las rutas
+  app.setGlobalPrefix('api/v1/juridicol');
+
+  // Seguridad de helmet
+  app.use(helmet());
+
+  // Logger de pino
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+  // Configuración de tuberia de validación global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Corta las propiedades que no formen parte del esquema definido
+      transform: true // Transforma los datos enviados por la red en objetos tipados segun sus clases DTO
+    })
+  )
+
   await app.listen(3000, () => {
     console.log(`Server listening on port 3000`);
   });
