76e5d6160f2c1eb87009a52ca5547b4c179d53c9
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-11-12 23:19:57 -0500

    Modificando modelo de archivo

---
+++ prisma/migrations/20241112030514_delete_tamanio_field_from_archivo_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tamanio` on the `Archivo` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Archivo" DROP COLUMN "tamanio";
